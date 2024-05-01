8386a7401e9d13ca2351068b56a33a66b44a0d3a
vladislavean <vladrollss228@gmail.com>
2024-05-01 12:25:41 +0300

    beggin translate

--- podcast_translation_bot/functions/__init__.py
+++ podcast_translation_bot/functions/__init__.py
@@ -1,1 +1,2 @@
 from .download_video import Video, VideoDownloader
+from .mp3_to_text_convert import ConvertToText, TranslateVideo
---
+++ podcast_translation_bot/functions/mp3_to_text_convert.py
@@ -0,0 +1,21 @@
+import speech_recognition as rc
+
+class TranslateVideo:
+    
+    def translate_text(self, mp3_file):
+        pass
+
+
+class ConvertToText:
+    
+    @staticmethod
+    def convert_mp3_to_text(file):
+        recognizer = rc.Recognizer()
+        with rc.AudioFile(file) as source:
+            audio = recognizer.record(source)
+            text = recognizer.recognize_google_cloud(audio_data=audio, language='ru-RU,en-US')
+        return text
+
+    def get_text(self, file):
+        pass
+
